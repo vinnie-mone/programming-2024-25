2e4708303e68cb78dacc5ef572bd8128ab1754dd
vinnie-mone <cutepie010101@gmail.com>
2024-11-04 03:20:54 +0700

    +

---
+++ 3практика/Class1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+//Базовый класс Бытовая техника
+    public class HomeAppliance
+    {
+        protected string model;
+        protected string manufacturer;
+        protected double price;
+        protected int year;
+
+    public HomeAppliance(string m = "Unknown", string manu = "Unknown", double p = 0.0, int y = 2000)
+    {
+        model = m;
+        manufacturer = manu;
+        price = p;
+        year = y;
+    }
+
+    public override string ToString()
+    {
+        return $"Model: {model}, Manufacturer: {manufacturer}, Price: {price} RUB, Year: {year}";
+    }
+
+    public interface ICloneable
+    {
+        object Clone();
+    }
+
+    public object Clone()
+    {
+        return new HomeAppliance(model,manufacturer,price,year);
+    }
+
+    public virtual void Print()
+    {
+        Console.WriteLine($"Model: {model}, Manufacturer: {manufacturer}, Price: {price} RUB, Year: {year}");
+    }
+
+    // Геттеры
+    public string GetModel { get { return model; } }
+    public string GetManufacturer { get { return manufacturer; } }
+    public double GetPrice { get { return price; } }
+    public int GetYear { get { return year; } }
+
+    // Сеттеры
+    public double SetPrice
+    {
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Price cannot be negative.");
+            else
+                price = value;
+        }
+    }
+    public int SetYear
+    {
+        set
+        {
+            int currentYear = 2024;
+            if (value < 1980 || value > currentYear)
+                throw new ArgumentException($"Year must be between 1980 and {currentYear}.");
+            else
+                year = value;
+        }
+    }
+}
+public class ApplianceComparer : IComparer<HomeAppliance>
+{
+    public int Compare(HomeAppliance x, HomeAppliance y)
+    {
+        if (x.GetYear != y.GetYear)
+            return 0;
+
+        else if (x.GetPrice != y.GetPrice)
+            return 0;
+
+        else if (x.GetModel != y.GetModel)
+            return 0;
+
+        else return 1;
+
+    }
+}
+
+
+/*public static IComparer SortYearAscending()
+{
+    return (IComparer)new SortYearAscendingHelper();
+}*/
+/*////
+
+
+
+using System;
+using System.Collections.Generic;
+
+public class ApplianceComparer : IComparer<HomeAppliance>
+{
+    public int Compare(HomeAppliance x, HomeAppliance y)
+    {
+        if (x == null && y != null)
+            return -1;
+        if (y == null && x != null)
+            return 1;
+        if (x == null && y == null)
+            return 0;
+
+        if (x.GetYear != y.GetYear)
+            return x.GetYear.CompareTo(y.GetYear);
+
+        if (x.GetPrice != y.GetPrice)
+            return x.GetPrice.CompareTo(y.GetPrice);
+
+        if (x.GetModel != y.GetModel)
+            return String.Compare(x.GetModel, y.GetModel);
+
+        return String.Compare(x.GetManufacturer, y.GetManufacturer);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<HomeAppliance> appliances = new List<HomeAppliance>()
+        {
+            new HomeAppliance("Washing Machine", "Samsung", 20000, 2021),
+            new HomeAppliance("Refrigerator", "LG", 60000, 2020),
+            new HomeAppliance("Air Conditioner", "Panasonic", 70000, 2022),
+            new HomeAppliance("Vacuum", "Xiaomi", 10000, 2023),
+            new HomeAppliance("Microwave Oven", "Whirlpool", 30000, 2020)
+        };
+
+        ApplianceComparer comparer = new ApplianceComparer();
+        appliances.Sort(comparer);
+
+        Console.WriteLine("Sorted by year descending:");
+        foreach (var appliance in appliances)
+        {
+            appliance.Print();
+        }
+
+        Console.WriteLine("\nSorted by price ascending:");
+        appliances.Sort((x, y) => Comparer<double>.Default.Compare(x.GetPrice, y.GetPrice));
+        foreach (var appliance in appliances)
+        {
+            appliance.Print();
+        }
+
+        Console.ReadKey();
+    }
+}*/
---
+++ 3практика/Program.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.Diagnostics;
+//using System.Collections.Generic;
+using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//Задание 4
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Создаем коллекцию и регистрируем обработчик события
+        ObservableCollection<HomeAppliance> appliances = new ObservableCollection<HomeAppliance>();
+        appliances.CollectionChanged += Appliances_CollectionChanged;
+
+        // Добавляем элементы в коллекцию
+        appliances.Add(new HomeAppliance("Model A", "Manufacturer X", 1000.0, 2020));
+        appliances.Add(new HomeAppliance("Model B", "Manufacturer Y", 2000.0, 2020));
+        appliances.Add(new HomeAppliance("Model C", "Manufacturer Z", 3000.0, 2018));
+        appliances.Add(new HomeAppliance("Model D", "Manufacturer W", 4000.0, 2019));
+
+        // Удаляем элемент из коллекции
+        Console.WriteLine("Удаление из колекции первого элемента");
+        appliances.RemoveAt(0);  // Удаляем первый элемент
+
+        // Выводим оставшиеся элементы
+        Console.WriteLine("Оставшиеся элементы");
+        foreach (var appliance in appliances)
+        {
+            appliance.Print();
+        }
+    }
+    private static void Appliances_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+    {
+        if (e.Action == NotifyCollectionChangedAction.Add)
+        {
+            foreach (HomeAppliance newAppliance in e.NewItems)
+            {
+                Console.WriteLine($"Added: {newAppliance}");
+            }
+        }
+        else if (e.Action == NotifyCollectionChangedAction.Remove)
+        {
+            foreach (HomeAppliance oldAppliance in e.OldItems)
+            {
+                Console.WriteLine($"Removed: {oldAppliance}");
+            }
+        }
+    }
+}
+
+//Задание 3
+/*class Program
+{
+    static void Main()
+    {
+        var appliances = new List<HomeAppliance>();
+        appliances.Add(new HomeAppliance("Model A", "Manufacturer X", 1000.0, 2020));
+        appliances.Add(new HomeAppliance("Model B", "Manufacturer Y", 2000.0, 2020));
+        appliances.Add(new HomeAppliance("Model C", "Manufacturer Z", 3000.0, 2018));
+        appliances.Add(new HomeAppliance("Model D", "Manufacturer W", 4000.0, 2019));
+
+        List<HomeAppliance> appliancess = new List<HomeAppliance>()
+        {
+            new HomeAppliance("Washing Machine", "Samsung", 20000, 2020),
+            new HomeAppliance("Refrigerator", "LG", 60000, 2020),
+            new HomeAppliance("Air Conditioner", "Panasonic", 70000, 2022),
+            new HomeAppliance("Vacuum", "Xiaomi", 10000, 2023),
+            new HomeAppliance("Microwave Oven", "Whirlpool", 30000, 2020)
+        };
+
+        ApplianceComparer comparer = new ApplianceComparer();
+        appliances.Sort(comparer);
+
+        // Сравнение двух объектов
+        var appA = appliancess[0];
+        var appB = appliancess[1];
+        //int comparisonResult = appA.CompareTo(appB);
+        //Console.WriteLine("Результат сравнения объектов: " + comparisonResult);
+
+        //int comparisonResult = ApplianceComparer.Compare(appliances[0], appliances[1]);
+        int comparisonResult = comparer.Compare(appA, appB);
+        Console.WriteLine("Результат сравнения объектов: " + comparisonResult);
+
+        // Глубокое копирование объекта
+        var clonedAppliance = (HomeAppliance)appliances[0].Clone();
+        Console.WriteLine("Результат копирования объектов: ");
+        Console.WriteLine("Объект исходный: ");
+        appliances[0].Print();
+        Console.WriteLine("Объект скопированный: ");
+        clonedAppliance.Print();
+
+        // Вывод коллекции
+        Console.WriteLine("Исходная коллекция:");
+        foreach (var appliance in appliances)
+        {
+            appliance.Print();
+        }
+
+        Console.ReadKey();
+    }
+}*/
+
+//Задание 2
+/*class Program
+{
+    static void Main()
+    {
+        int n = 1;
+        int searchValue = 4;
+
+        // a) Первая коллекция
+        Queue<int> queue = new Queue<int>();
+        queue.Enqueue(1);
+        queue.Enqueue(2);
+        queue.Enqueue(3);
+
+        // b) Вывод первой коллекции на консоль
+        Console.WriteLine("Первая коллекция:");
+        foreach (var item in queue)
+        {
+            Console.WriteLine(item);
+        }
+
+
+
+        // c) Удалить n элементов
+        while (n > 0)
+        {
+            queue.Dequeue();
+            n--;
+        }
+
+        // d) Добавить другие элементы
+        queue.Enqueue(4);
+        queue.Enqueue(5);
+
+        // e) Создание второй коллекции
+        List<int> list = new List<int>(queue);
+
+        // f) Вывести вторую коллекцию на консоль
+        Console.WriteLine("\nВторая коллекция:");
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
+
+        // g) Найти заданное значение
+        bool isFound = list.Exists(x => x == searchValue);
+        Console.WriteLine($"\nЗначение {searchValue} найдено: {isFound}");
+
+        Console.ReadKey();
+    }
+}
+*/
+
+//Задание 1
+/*namespace otter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string searchValue = "Лука";
+            int indexToRemove = 4;
+            // a) Создаем пустую коллекцию ArrayList
+            ArrayList list = new ArrayList();
+
+            // b) Заполняем список 5-ю случайными целыми числами
+            Random rnd = new Random();
+            for (int i = 0; i < 5; i++)
+                list.Add(rnd.Next(100));
+
+            // c) Добавляем строку
+            list.Add("Лука");
+
+            // d) Удаляем заданный элемент
+            list.RemoveAt(indexToRemove);
+
+            // e) Выводим количество элементов и коллекцию на консоль
+            Console.WriteLine($"Количество элементов: {list.Count}");
+            foreach (var item in list)
+                Console.Write($"{item}, ");
+
+            // f) Выполняем поиск в коллекции заданного значения
+            if (list.Contains(searchValue))
+                Console.WriteLine($"Элемент '{searchValue}' найден.");
+            else
+                Console.WriteLine($"Элемент '{searchValue}' не найден.");
+
+            Console.ReadKey();
+        }
+    }
+}*/
